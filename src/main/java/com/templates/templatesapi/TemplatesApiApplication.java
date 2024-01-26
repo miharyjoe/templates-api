35b9367fce11081c4dde3dacdd77a70ec1428ca3
miharyjoel <miharyjoel@gmail.com>
2024-01-26 09:40:34 +0300

    feat: init project

---
+++ src/main/java/com/templates/templatesapi/TemplatesApiApplication.java
@@ -0,0 +1,13 @@
+package com.templates.templatesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatesApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TemplatesApiApplication.class, args);
+  }
+
+}
